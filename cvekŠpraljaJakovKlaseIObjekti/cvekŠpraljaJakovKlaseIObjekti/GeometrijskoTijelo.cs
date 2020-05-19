8d80d2ccaa0f3ca313db3d10a83d97b4fd109300
Jakov764 <55740028+Jakov764@users.noreply.github.com>
2020-05-19 11:56:07 +0200

    Add files via upload

---
+++ cvekŠpraljaJakovKlaseIObjekti/cvekŠpraljaJakovKlaseIObjekti/GeometrijskoTijelo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cvekŠpraljaJakovKlaseIObjekti
+{
+    class GeometrijskoTijelo
+    {
+        double volumen;
+        double oplosje;
+        static int brojacTijela;
+
+        public double Volumen { get => volumen; set => volumen = value; }
+        public double Oplosje { get => oplosje; set => oplosje = value; }
+        public static int BrojacTijela { get => brojacTijela; set => brojacTijela = value; }
+
+        public GeometrijskoTijelo()
+        {
+            brojacTijela++;
+        }
+
+    }
+}
---
+++ cvekŠpraljaJakovKlaseIObjekti/cvekŠpraljaJakovKlaseIObjekti/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cvekŠpraljaJakovKlaseIObjekti
+
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+
+        {
+            Kugla x = new Kugla();
+            Kvadar y = new Kvadar();
+            Kocka z = new Kocka();
+            Console.WriteLine(GeometrijskoTijelo.BrojacTijela);
+            Console.ReadKey();
+        }
+
+    }
+}
